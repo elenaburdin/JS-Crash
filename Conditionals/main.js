693a0986d9d89ef6d4c67dadcaa2245249eb40eb
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-02-07 21:26:00 +0100

    Add files via upload

---
+++ Conditionals/main.js
@@ -0,0 +1,27 @@
+// Conditionals 
+
+// const x = 6;
+// const y = 11;
+
+// if (x > 5 && y > 10) {
+//     console.log('x is more than 5 or y is more than 10');
+// }
+
+
+const x = 11;
+const color = x > 10 ? 'red' : 'blue';
+// console.log(color);
+
+// Switch
+
+switch (color) {
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    default:
+        console.log('color is NOT red or blue');
+}
+
