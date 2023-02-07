f6d165fc1c3accf49cef850782f491987e4d7182
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-02-07 20:07:02 +0100

    Add files via upload

---
+++ datatypes/main.js
@@ -0,0 +1,13 @@
+// datatypes -> String, Number, Boolean, Null, Undefined, Symbol
+
+const name = 'John';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;  // undefined as well
+
+console.log(typeof z);
+
+
