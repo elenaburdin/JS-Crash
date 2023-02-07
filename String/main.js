84bb8137a392eb91f090ad29370b0d49673a087d
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-02-07 20:16:40 +0100

    Add files via upload

---
+++ String/main.js
@@ -0,0 +1,25 @@
+// String
+
+const firstName = 'John';
+const age = 30;
+
+// Concateation
+console.log('My name is ' + firstName + ', and I am ' + age);
+
+// Template String
+const hello = `My name is ${firstName}, and I am ${age}`;
+console.log(hello);
+
+// String Property and Methods
+const s = 'Hello World!';
+console.log(s.length);
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.substring(0, 5).toUpperCase());
+console.log(s.split(''));
+
+const i = 'technology, computers, it, code';
+console.log(i.split(', '));
+
+
+
