78f7cf297d86f5ec0356466548ff41dc75f32bed
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-02-08 09:58:06 +0100

    Add files via upload

---
+++ DOM/main.js
@@ -0,0 +1,11 @@
+// DOM
+// single element selectors
+console.log(document.getElementById('my-form'));
+console.log(document.querySelector('h1'));
+
+
+// multiple element selectors
+console.log(document.querySelectorAll('.item'));
+console.log(document.getElementsByClassName('item'));
+console.log(document.getElementsByTagName('li'));
+
